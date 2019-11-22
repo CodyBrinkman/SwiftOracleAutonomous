a79217d11de468763041ee0a52c8b996fa12429e
Cody Brinkman <55295461+CodyBrinkman@users.noreply.github.com>
2019-11-22 10:56:54 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftOracleAutonomous",
-    targets: [Target(name: "SwiftOracleAutonomous", dependencies: ["cocilib"])],
+    targets: [Target(name: "SwiftOracle", dependencies: ["cocilib"])],
     dependencies: [
         
     ]
